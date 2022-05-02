b4822b515a70db1a6f3384ab8a00ef1ba74ebd49
sureste <edipean@hotmail.com>
2022-05-02 17:43:20 -0500

    NotpiDogs

---
+++ client/src/Components/landingPage/landingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function LandingPage(props) {
+    return (
+        <div>
+           miau
+        </div>)
+}
+export default LandingPage
